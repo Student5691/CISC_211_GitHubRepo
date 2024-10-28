feb792c33a6bb2fa3aa52b0e77f41511014baf4e
Mason L <sux462@gmail.com>
2024-10-28 13:27:33 -0700

    removed redundant lines that set ebx and edx to 1 for the newline print statement; both registers are already 1 from the result print statement

--- Quiz3/quiz3.asm
+++ Quiz3/quiz3.asm
@@ -5,7 +5,8 @@ _start:
         xor eax, eax            ;clear registers
         xor ebx, ebx
         xor edx, edx
-        xor ecx, ecx                    
+        xor ecx, ecx
+
         mov ecx, [fact]         ;set number of the dersired factorial calculation, 5 in this case
         dec ecx                 ;initial decrement since eax will hold the starting value, 5
         mov eax, [fact]         ;set eax to the first value in the factorial sequence
@@ -23,9 +24,7 @@ factorialCalc:
         int 0x80
 
         mov eax, 4              ;print sequence for a new line character
-        mov ebx, 1
         mov ecx, newLine
-        mov edx, 1
         int 0x80
 
         mov eax, 1              ;terminate the program
